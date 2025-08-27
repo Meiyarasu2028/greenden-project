3d0a6811d93425ed0cdde210b97394f1b9dc30b6
Meiyarasu <meiysrn06@gmail.com>
2025-08-27 02:42:06 -0700

    Add js

---
+++ product.js
@@ -0,0 +1,24 @@
+
+var prodectcontainer = document.getElementById("prodect-container")
+var search = document.getElementById("search")
+var prodectlist = prodectcontainer.querySelectorAll("div")
+
+search.addEventListener("keyup",function(){
+
+    var enteredValue = event.target.value.toUpperCase()
+
+    for(count=0; count<prodectlist.length; count=count+1){
+
+        var prodectname = prodectlist[count].querySelector("h2").textContent.toUpperCase()
+
+        if(prodectname.indexOf(enteredValue)<0){
+
+            prodectlist[count].style.display="none"
+        }
+
+        else{
+            prodectlist[count].style.display="block"
+        }
+    }
+})
+
